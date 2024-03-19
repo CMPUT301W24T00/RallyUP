48805b3264df034a529ed4c89865d4922b6fec09
kchrisalis <kayecryz@ualberta.ca>
2024-03-19 17:28:50 -0600

    Added most pages
    - still missing event details page
    - still need to add list adaptors

---
+++ project/app/src/main/java/com/example/rallyup/uiReference/admin/AdminBrowseEventsActivity.java
@@ -0,0 +1,29 @@
+package com.example.rallyup.uiReference.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import com.example.rallyup.R;
+import com.example.rallyup.uiReference.splashScreen;
+
+public class AdminBrowseEventsActivity extends AppCompatActivity {
+
+    ImageButton backBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_browse_events);
+
+        backBtn = findViewById(R.id.admin_events_back_button);
+
+        // back button to return to previous page
+        backBtn.setOnClickListener(view -> {
+            Intent intent = new Intent(getBaseContext(), AdminHomepageActivity.class);  // placeholder for attendee opener
+            startActivity(intent);
+        });
+    }
+}
